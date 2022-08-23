4bc8d793c8b58e471ad89055813264e6879ce936
NFSL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2022-08-23 23:34:49 +0800

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,15 +1,28 @@
 import os
 import sys
 
+RENAME_LIST_EN = {
+    ["I.MingCP", "PMing.I"],
+    ["I.MingCP", "I.MingU"],
+    ["I.MingVarCP", "PMingVar.I"],
+    ["I.MingVarCP", "I.MingVarU"],
+}
+
+
 def main():
     file_list = []
+    # walk through all directory of current repo
     for root, dirs, files in os.walk("."):
-       for name in files:
-            if "I.MingCP-" in name or "I.MingVarCP-" in name:
-                file_list.append(os.path.join(root, name))
-       #   print(os.path.join(root, name))
-       #for name in dirs:
-           
+        # list all files in current walking dir
+        for name in files:
+            # if name
+            if "I.MingCP" in name or "I.MingVarCP" in name:
+                # loop through changing name
+                for orig, new in RENAME_LIST_EN:
+                    # if file does not exist
+                    if not os.path.exists(os.path.join(root, name.replace(orig, new))):
+                        # add file into list of files to be convert
+                        file_list.append(os.path.join(root, name))
                
     print(file_list)
     sys.exit(0)
