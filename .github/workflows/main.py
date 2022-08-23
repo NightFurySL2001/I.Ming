7457d293b3cb43779cde7244a103a634809366d3
NFSL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2022-08-23 23:13:38 +0800

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 def main():
-    for root, dirs, files in os.walk("../.."):
+    print(os.getcwd())
+    for root, dirs, files in os.walk("."):
        for name in files:
           print(os.path.join(root, name))
        for name in dirs:
