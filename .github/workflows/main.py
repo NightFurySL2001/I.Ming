268622911d886e68b75cb88b008b4b74f5d9473d
NFSL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2022-08-23 23:25:51 +0800

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -4,11 +4,13 @@ import sys
 def main():
     file_list = []
     for root, dirs, files in os.walk("."):
-       #for name in files:
+       for name in files:
+            if "I.MingCP-" in name or "I.MingVarCP-" in name:
+                file_list.append(os.path.join(root, name))
        #   print(os.path.join(root, name))
-       for name in dirs:
-           if "I.MingCP-" in name or "I.MingVarCP-" in name:
-               file_list.append(os.path.join(root, name))
+       #for name in dirs:
+           
+               
     print(file_list)
     sys.exit(0)
 
